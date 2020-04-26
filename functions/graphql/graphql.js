7abb502560c02970bc9fe0734bf2c3c745fae5f4
HAINES-DESKTOP\richa <richardkhaines@hotmail.co.uk>
2020-04-26 19:55:35 +0200

    gotta work

--- functions/graphql/graphql.js
+++ functions/graphql/graphql.js
@@ -1,26 +1,9 @@
-// const {ApolloServer} = require('apollo-server-lambda');
-const { createHttpLink } = require('apollo-link-http');
-const { ApolloServer, makeRemoteExecutableSchema, introspectSchema } = require('apollo-server-micro');
+const {ApolloServer} = require('apollo-server-lambda');
+const { typeDefs } = require('./apollo-typeDefs');
 const { client, query } = require('./db');
-const fetch = require('isomorphic-fetch');
-
-
-const link = createHttpLink({
-    uri: 'https://graphql.fauna.com/graphql',
-    fetch,
-    headers: {
-      Authorization: `Bearer ${process.env.CLIENT_KEY}`,
-    },
-  });
-
-const schema = makeRemoteExecutableSchema({
-    schema: introspectSchema(link),
-    link
-});
-
 
 const server = new ApolloServer({
-    schema,
+    typeDefs,
     context: ({event}) => {
         return {client, query, headers: event.headers};
     },
